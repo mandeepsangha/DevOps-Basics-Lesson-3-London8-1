d64c068a6559b78d43eed0b16002ae9d0ea28ca3
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-04-23 14:10:55 +0100

    logging

--- routes/handlers.js
+++ routes/handlers.js
@@ -3,14 +3,16 @@ let request_number = 0;
 const indexHandler = (req, res) => {
   var current_time = new Date();
   request_number += 1;
-
-  res.send(`Hello! The current server time in is ${current_time}. The server has responded to ${request_number} requests since it started.`)
-}
+  console.log(`${current_time} Request no.${request_number}`);
+  res.send(
+    `Hello! The current server time in is ${current_time}. The server has responded to ${request_number} requests since it started.`
+  );
+};
 
 const sayHelloHandler = (req, res) => {
   const name = req.params.name || "person";
-
+  console.log(`${current_time} ${name}`);
   res.send(`Hello ${name}!`);
-}
+};
 
 module.exports = { indexHandler, sayHelloHandler };

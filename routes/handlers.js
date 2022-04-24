543b3c4f45de40734be068253ce08503a589dade
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-04-24 20:19:29 +0100

    Added pino-pretty

--- routes/handlers.js
+++ routes/handlers.js
@@ -1,9 +1,22 @@
 let request_number = 0;
 
+const pino = require(`pino`); // Import the Pino Library
+const { default: PinoPretty } = require("pino-pretty");
+
+const logger = pino({
+  transport: {
+    target: `pino-pretty`,
+    options: { translateTime: true },
+  },
+});
+
 const indexHandler = (req, res) => {
   var current_time = new Date();
   request_number += 1;
-  console.log(`${current_time} Request no.${request_number}`);
+  logger.info(`${current_time} Request no.${request_number}`);
+
+  //console.log(`${current_time} Request no.${request_number}`);
+
   res.send(
     `Hello! The current server time in is ${current_time}. The server has responded to ${request_number} requests since it started.`
   );
@@ -11,7 +24,9 @@ const indexHandler = (req, res) => {
 
 const sayHelloHandler = (req, res) => {
   const name = req.params.name || "person";
-  console.log(`${current_time} ${name}`);
+
+  logger.info(`${current_time} ${name}`);
+  //console.log(`${current_time} ${name}`);
   res.send(`Hello ${name}!`);
 };
 
